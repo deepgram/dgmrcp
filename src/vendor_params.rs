9b4b4dd339200c57d1bbdda020657d1793ff0b82
chesedo <pieter@chesedo.me>
2021-10-05 14:56:49 +0200

    Add tests for vendor_params

--- src/vendor_params.rs
+++ src/vendor_params.rs
@@ -158,3 +158,157 @@ impl<'de, 'a> MapAccess<'de> for KeyValueList<'a> {
         result
     }
 }
+
+#[derive(Default, Deserialize)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
+pub struct VendorHeaders {
+    #[serde(rename = "com.deepgram.model")]
+    pub model: Option<String>,
+
+    #[serde(rename = "com.deepgram.numerals")]
+    pub numerals: Option<bool>,
+
+    #[serde(rename = "com.deepgram.ner")]
+    pub ner: Option<bool>,
+
+    #[serde(rename = "com.deepgram.no_delay")]
+    pub no_delay: Option<bool>,
+
+    #[serde(rename = "com.deepgram.plugin")]
+    pub plugin: Option<String>,
+
+    #[serde(rename = "com.deepgram.keywords")]
+    pub keywords: Option<String>,
+
+    #[serde(rename = "com.deepgram.keyword_boost")]
+    pub keyword_boost: Option<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ffi;
+    use pretty_assertions::assert_eq;
+
+    /// Wrapper for `apt_pair_arr_t`
+    pub struct AptPairArray {
+        allocator: *mut ffi::apr_allocator_t,
+        pool: *mut ffi::apr_pool_t,
+        array: *mut ffi::apt_pair_arr_t,
+    }
+
+    impl AptPairArray {
+        /// Creates a new pair array
+        pub fn new() -> Self {
+            use std::mem::MaybeUninit;
+
+            let mut tmp = AptPairArray {
+                allocator: unsafe { MaybeUninit::uninit().assume_init() },
+                pool: unsafe { MaybeUninit::uninit().assume_init() },
+                array: unsafe { MaybeUninit::uninit().assume_init() },
+            };
+
+            unsafe {
+                ffi::apr_initialize();
+                ffi::apr_allocator_create(&mut tmp.allocator);
+                ffi::apr_pool_create_unmanaged_ex(&mut tmp.pool, None, tmp.allocator);
+                tmp.array = ffi::apt_pair_array_create(5, tmp.pool);
+            };
+
+            tmp
+        }
+
+        /// Add a new entry to the array pair
+        pub fn add(&mut self, name: String, value: String) {
+            use std::ffi::CString;
+            let c_name = CString::new(name).expect("Name should not contain a \0 character");
+            let c_value = CString::new(value).expect("Value should not contain a \0 character");
+
+            let apt_name = &ffi::apt_str_t {
+                length: c_name.as_bytes().len(),
+                buf: c_name.into_raw(),
+            };
+            let apt_value = &ffi::apt_str_t {
+                length: c_value.as_bytes().len(),
+                buf: c_value.into_raw(),
+            };
+
+            unsafe {
+                ffi::apt_pair_array_append(self.array, apt_name, apt_value, self.pool);
+            }
+
+            // Claim back, else a segmentation fault will be caused
+            let _ = unsafe { CString::from_raw(apt_name.buf) };
+            let _ = unsafe { CString::from_raw(apt_value.buf) };
+        }
+    }
+
+    // Clean up C resources when done
+    impl Drop for AptPairArray {
+        fn drop(&mut self) {
+            unsafe {
+                ffi::apr_pool_destroy(self.pool);
+                ffi::apr_allocator_destroy(self.allocator);
+                ffi::apr_terminate();
+            }
+        }
+    }
+
+    #[test]
+    fn parse_no_value() -> Result<()> {
+        let arr = AptPairArray::new();
+        let actual: VendorHeaders = unsafe { from_header_array(arr.array)? };
+        let expected = VendorHeaders::default();
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn parse_all_values() -> Result<()> {
+        let mut arr = AptPairArray::new();
+        arr.add("com.deepgram.model".to_string(), "basic".to_string());
+        arr.add("com.deepgram.numerals".to_string(), "false".to_string());
+        arr.add("com.deepgram.ner".to_string(), "true".to_string());
+        arr.add("com.deepgram.no_delay".to_string(), "true".to_string());
+        arr.add(
+            "com.deepgram.plugin".to_string(),
+            "noise,static".to_string(),
+        );
+        arr.add("com.deepgram.keywords".to_string(), "property".to_string());
+        arr.add(
+            "com.deepgram.keyword_boost".to_string(),
+            "agent".to_string(),
+        );
+
+        let actual: VendorHeaders = unsafe { from_header_array(arr.array)? };
+        let expected = VendorHeaders {
+            model: Some("basic".to_string()),
+            numerals: Some(false),
+            ner: Some(true),
+            no_delay: Some(true),
+            plugin: Some("noise,static".to_string()),
+            keywords: Some("property".to_string()),
+            keyword_boost: Some("agent".to_string()),
+        };
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn parse_extra_values_are_ignored() -> Result<()> {
+        let mut arr = AptPairArray::new();
+        arr.add("com.deepgram.unknown".to_string(), "true".to_string());
+        arr.add("com.other.model".to_string(), "basic".to_string());
+
+        let actual: VendorHeaders = unsafe { from_header_array(arr.array)? };
+        let expected = VendorHeaders::default();
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+}
